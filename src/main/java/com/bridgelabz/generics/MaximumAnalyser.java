db65d52f646ded9b310846193358efa2c4782ccc
Preranashinde <preranapshinde3009@gmail.com>
2020-04-15 17:02:56 -0300

    "REFACTORED:To create generic class to take in three variables of generic type"

--- src/main/java/com/bridgelabz/generics/MaximumAnalyser.java
+++ src/main/java/com/bridgelabz/generics/MaximumAnalyser.java
@@ -1,13 +1,28 @@
 package com.bridgelabz.generics;
 
-public class MaximumAnalyser {
-    public <K extends Comparable<K>> K findMaximumNumber(K first, K second, K third)
-    {
+public class MaximumAnalyser<K extends Comparable<K>>  {
+    private K first;
+    private K second;
+    private K third;
+
+    //Default Constructor
+    public MaximumAnalyser() {
+    }
+    //Parameterized Constructor
+    public MaximumAnalyser(K first, K second, K third){
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+    public K findMaximumNumber(K first, K second, K third)    {
         if (first.compareTo(second) > 0 && first.compareTo(third) > 0)
             return first;
         else if (second.compareTo(first) > 0 && second.compareTo(third) > 0)
             return second;
         else
             return third;
+    }
+    public <T extends Comparable<T>> T findMaximum(){
+        return (T) findMaximumNumber(first, second, third);
     }
 }
--- src/test/java/com/bridgelabz/generics/MaximumAnalyserTest.java
+++ src/test/java/com/bridgelabz/generics/MaximumAnalyserTest.java
@@ -7,54 +7,107 @@ public class MaximumAnalyserTest {
     MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
     @Test
     public void GivenThreeIntegers_findMaximumAt_firstPosition() {
-        Integer maximumNumber = maximumAnalyser.findMaximumNumber(101,21,51);
+        Integer maximumNumber = (Integer) maximumAnalyser.findMaximumNumber(101,21,51);
         Assert.assertEquals((Integer) 101 , maximumNumber);
     }
     @Test
     public void GivenThreeIntegers_findMaximumAt_secondPosition() {
-        Integer maximumNumber = maximumAnalyser.findMaximumNumber(21 ,101, 51);
+        Integer maximumNumber =(Integer) maximumAnalyser.findMaximumNumber(21 ,101, 51);
         Assert.assertEquals((Integer) 101 , maximumNumber);
     }
     @Test
     public void GivenThreeIntegers_findMaximumAt_thirdPosition() {
         MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
-        Integer maximumNumber = maximumAnalyser.findMaximumNumber(21 ,51, 101);
+        Integer maximumNumber =(Integer) maximumAnalyser.findMaximumNumber(21 ,51, 101);
         Assert.assertEquals((Integer) 101 , maximumNumber);
     }
     @Test
     public void GivenThreeFloat_findMaximumAt_firstPosition_shuldReturnSameNumber() {
         MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
-        Float maximumNumber = maximumAnalyser.findMaximumNumber(101.05f,21.50f, 51.10f);
+        Float maximumNumber =(Float)maximumAnalyser.findMaximumNumber(101.05f,21.50f, 51.10f);
         Assert.assertEquals((Float) 101.05f , maximumNumber);
     }
     @Test
     public void GivenThreeFloat_findMaximumAt_secondPosition_shuldReturnSameNumber() {
         MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
-        Float maximumNumber = maximumAnalyser.findMaximumNumber(21.50f ,101.05f, 51.10f);
+        Float maximumNumber =(Float)maximumAnalyser.findMaximumNumber(21.50f ,101.05f, 51.10f);
         Assert.assertEquals((Float) 101.05f , maximumNumber);
     }
     @Test
     public void GivenThreeFloat_findMaximumAt_thirdPosition_shuldReturnSameNumber() {
         MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
-        Float maximumNumber = maximumAnalyser.findMaximumNumber(51.10f ,21.50f, 101.05f);
+        Float maximumNumber =(Float) maximumAnalyser.findMaximumNumber(51.10f ,21.50f, 101.05f);
         Assert.assertEquals((Float) 101.05f , maximumNumber);
     }
     @Test
     public void GivenThreeStrings_findMaximumAt_firstPosition_shuldReturnSameString() {
         MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
-        String maximumString = maximumAnalyser.findMaximumNumber("Silver" , "Gold" , "Iron");
+        String maximumString =(String) maximumAnalyser.findMaximumNumber("Silver" , "Gold" , "Iron");
         Assert.assertEquals((String) "Silver" , maximumString);
     }
     @Test
     public void GivenThreeStrings_findMaximumAt_secondPosition_shuldReturnSameString() {
         MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
-        String maximumString = maximumAnalyser.findMaximumNumber("Gold","Silver" ,"Iron");
+        String maximumString =(String) maximumAnalyser.findMaximumNumber("Gold","Silver" ,"Iron");
         Assert.assertEquals((String) "Silver" , maximumString);
     }
     @Test
     public void GivenThreeStrings_findMaximumAt_thirdPosition_shuldReturnSameString() {
         MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
-        String maximumString = maximumAnalyser.findMaximumNumber("Iron" ,"Gold", "Silver");
+        String maximumString =(String) maximumAnalyser.findMaximumNumber("Iron" ,"Gold", "Silver");
+        Assert.assertEquals((String) "Silver" , maximumString);
+    }
+
+    @Test
+    public void GivenThreeIntegers_findMaximumAt_firstPosition_usingGenericClass() {
+        Integer maximumNumber = (Integer) maximumAnalyser.findMaximumNumber(101,21,51);
+        Assert.assertEquals((Integer) 101 , maximumNumber);
+    }
+    @Test
+    public void GivenThreeIntegers_findMaximumAt_secondPosition_usingGenericClass() {
+        Integer maximumNumber =(Integer) maximumAnalyser.findMaximumNumber(21 ,101, 51);
+        Assert.assertEquals((Integer) 101 , maximumNumber);
+    }
+    @Test
+    public void GivenThreeIntegers_findMaximumAt_thirdPosition_usingGenericClass() {
+        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
+        Integer maximumNumber =(Integer) maximumAnalyser.findMaximumNumber(21 ,51, 101);
+        Assert.assertEquals((Integer) 101 , maximumNumber);
+    }
+    @Test
+    public void GivenThreeFloat_findMaximumAt_firstPosition_shuldReturnSameNumber_usingGenericClass() {
+        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
+        Float maximumNumber =(Float)maximumAnalyser.findMaximumNumber(101.05f,21.50f, 51.10f);
+        Assert.assertEquals((Float) 101.05f , maximumNumber);
+    }
+    @Test
+    public void GivenThreeFloat_findMaximumAt_secondPosition_shuldReturnSameNumber_usingGenericClass() {
+        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
+        Float maximumNumber =(Float)maximumAnalyser.findMaximumNumber(21.50f ,101.05f, 51.10f);
+        Assert.assertEquals((Float) 101.05f , maximumNumber);
+    }
+    @Test
+    public void GivenThreeFloat_findMaximumAt_thirdPosition_shuldReturnSameNumber_usingGenericClass() {
+        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
+        Float maximumNumber =(Float) maximumAnalyser.findMaximumNumber(51.10f ,21.50f, 101.05f);
+        Assert.assertEquals((Float) 101.05f , maximumNumber);
+    }
+    @Test
+    public void GivenThreeStrings_findMaximumAt_firstPosition_shuldReturnSameString_usingGenericClass() {
+        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
+        String maximumString =(String) maximumAnalyser.findMaximumNumber("Silver" , "Gold" , "Iron");
+        Assert.assertEquals((String) "Silver" , maximumString);
+    }
+    @Test
+    public void GivenThreeStrings_findMaximumAt_secondPosition_shuldReturnSameString_usingGenericClass() {
+        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
+        String maximumString =(String) maximumAnalyser.findMaximumNumber("Gold","Silver" ,"Iron");
+        Assert.assertEquals((String) "Silver" , maximumString);
+    }
+    @Test
+    public void GivenThreeStrings_findMaximumAt_thirdPosition_shuldReturnSameString_usingGenericClass() {
+        MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
+        String maximumString =(String) maximumAnalyser.findMaximumNumber("Iron" ,"Gold", "Silver");
         Assert.assertEquals((String) "Silver" , maximumString);
     }
 }
